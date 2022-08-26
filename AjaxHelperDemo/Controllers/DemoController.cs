5fed6beaf157bddb903377fa01584c778ecfb329
HP <HP@DESKTOP-G4J7E8F>
2022-08-26 15:07:25 +0530

    Add project files.

---
+++ AjaxHelperDemo/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxHelperDemo.Models;
+
+namespace AjaxHelperDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(Emp emp)
+        {
+            return Json("true",JsonRequestBehavior.AllowGet);   
+        }
+    }
+}
---
+++ AjaxHelperDemo/Models/Emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxHelperDemo.Models
+{
+    public class Emp
+    {
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+    }
+}
